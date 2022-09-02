7171c286c018f72f5c48b2d0f5959cce5a7f9d3e
pattrie <patri.b.morais@gmail.com>
2022-09-01 21:41:57 -0300

    add new test

--- src/test/java/br/com/mba/inventorycontrol/service/ProductServiceTest.java
+++ src/test/java/br/com/mba/inventorycontrol/service/ProductServiceTest.java
@@ -12,11 +12,10 @@ import br.com.mba.inventorycontrol.repository.ProductInRepository;
 import br.com.mba.inventorycontrol.repository.ProductRepository;
 import br.com.mba.inventorycontrol.repository.StoragePlaceRepository;
 import br.com.mba.inventorycontrol.repository.SupplierRepository;
-import br.com.mba.inventorycontrol.template.AddressTemplate;
 import br.com.mba.inventorycontrol.template.CategoryTemplate;
 import br.com.mba.inventorycontrol.template.ProductInTemplate;
+import br.com.mba.inventorycontrol.template.ProductRequestDtoTemplate;
 import br.com.mba.inventorycontrol.template.ProductTemplate;
-import br.com.mba.inventorycontrol.template.StoragePlaceTemplate;
 import br.com.mba.inventorycontrol.template.SupplierTemplate;
 import java.util.Optional;
 import lombok.val;
@@ -57,6 +56,26 @@ class ProductServiceTest {
     when(productInRepository.save(any())).thenReturn(productIn);
 
     val savedProduct = productService.create(PRODUCT);
+
+    assertNotNull(savedProduct);
+  }
+
+  @Test
+  void shouldCreateNewProductWithNullIds() {
+    val category = CategoryTemplate.CATEGORY;
+    val supplier = SupplierTemplate.SUPPLIER;
+    val product = ProductTemplate.PRODUCT;
+    val productRequestDto = ProductRequestDtoTemplate.PRODUCT;
+    productRequestDto.getCategory().setId(null);
+    productRequestDto.getSupplier().setId(null);
+    val productIn = ProductInTemplate.PRODUCT_IN;
+
+    when(categoryRepository.save(any())).thenReturn(category);
+    when(supplierRepository.save(any())).thenReturn(supplier);
+    when(productRepository.save(any())).thenReturn(product);
+    when(productInRepository.save(any())).thenReturn(productIn);
+
+    val savedProduct = productService.create(productRequestDto);
 
     assertNotNull(savedProduct);
   }
